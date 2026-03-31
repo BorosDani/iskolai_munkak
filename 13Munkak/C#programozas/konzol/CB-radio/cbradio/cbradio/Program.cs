f7d759c628a408ce69dd0186802102d213789f8f
Dani <103130240+BorosDani@users.noreply.github.com>
2026-03-31 19:32:51 +0200

    Add files via upload

---
+++ 13Munkak/C#programozas/konzol/CB-radio/cbradio/cbradio/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//38 pont ---> 40 pontból
+//1. Feladat
+namespace cbradio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cbradio();
+            Console.ReadLine();
+        }
+        private static void cbradio() 
+        {
+            feladat2();
+            feladat3();
+            Console.WriteLine("\n");
+            feladat4();
+            Console.WriteLine("\n");
+            feladat5();
+            Console.WriteLine("\n");
+            feladat7();
+            feladat8();
+            Console.WriteLine("\n");
+            feladat9();
+        }
+
+        struct CBRadioStruktura
+        {
+            public int Ora;
+            public int Perc;
+            public int AdasDb;
+            public string Nev;
+        }
+        static List<CBRadioStruktura> adatok = new List<CBRadioStruktura>();
+
+        private static void feladat2() 
+        {
+            StreamReader olvasocsatorna = new StreamReader("cb.txt");
+            string elsosor = olvasocsatorna.ReadLine();
+            string sor;
+
+            string[] darabol;
+
+            while (!olvasocsatorna.EndOfStream) 
+            {
+                sor = olvasocsatorna.ReadLine();
+                darabol = sor.Split(';');
+
+                CBRadioStruktura radio = new CBRadioStruktura();
+
+                radio.Ora = int.Parse(darabol[0]);
+                radio.Perc = int.Parse(darabol[1]);
+                radio.AdasDb = int.Parse(darabol[2]);
+                radio.Nev = darabol[3];
+
+                adatok.Add(radio);
+            }
+        }
+
+        private static void feladat3() 
+        {
+            int bejegyzesek_szama = 0;
+            for (int i = 0; i < adatok.Count(); i++)
+            {
+                bejegyzesek_szama++;
+            }
+
+            Console.WriteLine("3. Feladat: Bejegyzések száma: {0} db", bejegyzesek_szama);
+        }
+
+        private static void feladat4() 
+        {
+            int index = 0;
+            bool egyperc_negyadas = false;
+            while (adatok[index].AdasDb != 4) 
+            {
+                index++;
+                if (adatok[index].AdasDb == 4) 
+                {
+                    egyperc_negyadas = true;
+                }
+            }
+
+            if (egyperc_negyadas)
+            {
+                Console.WriteLine("4. Feladat: Volt négy adást indító sofőr.");
+            }
+            else 
+            {
+                Console.WriteLine("4. Feladat: Nem volt négy adást indító sofőr.");
+            }
+        }
+
+        private static void feladat5() 
+        {
+            Console.Write("5. Feladat: Kérek egy nevet: ");
+            string sofor_nev = Console.ReadLine();
+            bool van_ilyen_nevu = false;
+            int adas_hasznalatanak_szama = 0;
+
+            for (int i = 0; i < adatok.Count(); i++)
+            {
+                if (adatok[i].Nev.ToLower() == sofor_nev.ToLower())
+                {
+                    van_ilyen_nevu = true;
+                    adas_hasznalatanak_szama += adatok[i].AdasDb;
+                }
+            }
+            if (van_ilyen_nevu)
+            {
+                Console.WriteLine($"\t{sofor_nev} {adas_hasznalatanak_szama}x használta a CB-rádiót.");
+            }
+            else 
+            {
+                Console.WriteLine("\tNincs ilyen nevű sofőr!");
+            }
+        }
+
+        //6. Feladat
+        static int AtszamolPercre(int ora, int perc)
+        {
+            return (ora * 60) + perc;
+        }
+
+        private static void feladat7() 
+        {
+            StreamWriter irocsatorna = new StreamWriter("cb2.txt");
+            irocsatorna.WriteLine("Kezdes;Nev;AdasDb");
+
+            int kezdes_percben;
+            string uj_sor;
+            for (int i = 0; i < adatok.Count(); i++)
+            {
+                kezdes_percben = AtszamolPercre(adatok[i].Ora, adatok[i].Perc);
+                uj_sor = $"{kezdes_percben};{adatok[i].Nev};{adatok[i].AdasDb}";
+                irocsatorna.WriteLine(uj_sor);
+            }
+
+            irocsatorna.Close();
+        }
+
+        //------------ HASH SET-tel kellene megcsinálni ------------
+        private static void feladat8() 
+        {
+            List<string> nevek = new List<string>();
+            int soforok_szama = 0;
+            bool uj_nev;
+
+            for (int i = 0; i < adatok.Count(); i++)
+            {
+                uj_nev = true;
+
+                for (int j = 0; j < nevek.Count(); j++)
+                {
+                    if (adatok[i].Nev == nevek[j]) 
+                    {
+                        uj_nev = false;
+                    }
+                }
+
+                if (uj_nev) 
+                {
+                    nevek.Add(adatok[i].Nev);
+                    soforok_szama++;
+                }
+            }
+            Console.WriteLine($"8. Feladat: Sofőrök száma: {soforok_szama} fő");
+        }
+
+        private static void feladat9() 
+        {
+            List<string> nevek = new List<string>();
+            List<int> ossz_adasok = new List<int>();
+            bool uj_nev;
+
+            for (int i = 0; i < adatok.Count(); i++)
+            {
+                uj_nev = true;
+
+                for (int j = 0; j < nevek.Count(); j++)
+                {
+                    if (adatok[i].Nev == nevek[j])
+                    {
+                        uj_nev = false;
+                        ossz_adasok[j] += adatok[i].AdasDb;
+                    }
+                }
+
+                if (uj_nev)
+                {
+                    nevek.Add(adatok[i].Nev);
+                    ossz_adasok.Add(adatok[i].AdasDb);
+                }
+            }
+
+            int max_index = 0;
+
+            for (int i = 0; i < ossz_adasok.Count(); i++)
+            {
+                if (ossz_adasok[i] > ossz_adasok[max_index]) 
+                {
+                    max_index = i;
+                }
+            }
+            Console.WriteLine($"9. Feladat: Legtöbb adást indító sofőr\n\t" +
+                $"Név: {nevek[max_index]}\n\t" +
+                $"Adások száma: {ossz_adasok[max_index]} alkalom");
+        }
+    }
+}
---
+++ 13Munkak/C#programozas/konzol/ultrabalaton_hibaskodok/Hibas_Ultrabalaton_KG/Ultrabalaton/Ultrabalaton/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultrabalaton
+{
+    class Program
+    {
+        struct versenyzoStruktura
+        {
+            public string nev;
+            public int rajtszam; 
+            public string kategoria;
+            public string versenyido; //int[]
+            public int tavSzazalek;
+            public int ora;
+            public int perc;
+            public int masodperc;
+        }
+        static List<versenyzoStruktura> adatok = new List<versenyzoStruktura>();
+        static void Main(string[] args)
+        {
+            StreamReader olvasocsatorna = new StreamReader("ub2017egyeni.txt");
+            string elsosor = olvasocsatorna.ReadLine();
+            string sor;
+
+            string[] darabol;
+            while(!olvasocsatorna.EndOfStream)
+            {
+                sor = olvasocsatorna.ReadLine();
+                darabol = sor.Split(';');
+
+
+                versenyzoStruktura versenyzo = new versenyzoStruktura();
+
+                versenyzo.nev = darabol[0];
+                versenyzo.rajtszam = int.Parse(darabol[1]);
+                versenyzo.kategoria = darabol[2];
+                //versenyzo.versenyido = darabol[3];
+                versenyzo.tavSzazalek = int.Parse(darabol[4]);
+
+                string[] idoDarabol = darabol[3].Split(':');
+                versenyzo.ora = int.Parse(idoDarabol[0]);
+                versenyzo.perc = int.Parse(idoDarabol[1]);
+                versenyzo.masodperc = int.Parse(idoDarabol[2]);
+
+                adatok.Add(versenyzo);
+            }
+            olvasocsatorna.Close();
+            //32 pont
+
+            //3. Feladat: Mennyi versenyző van összesen
+            Feladat03();
+
+            //4. Feladat: Mennyi női versenyző van összesen
+            Feladat04();
+
+
+            //5. Feladat: Indult-e a keresett versenyző a versenyen
+            Feladat05();
+
+            //6. Feladat: Versenyidő órában
+            Feladat06();
+            //7. Feladat: Férfiak átlag versenyideje
+            Feladat07();
+            //8. Feladat: Legjobbak mindkét kategóriából
+            //Feladat08();
+            Console.ReadKey();
+
+        }
+
+        private static void Feladat08()//1,0,0,1,0,0,0
+        {
+            string LegjobbFerfiVersenyzo;
+            int LegjobbFerfiRajtszam;
+            int LegjobbFerfiOra;
+            int LegjobbFerfiPerc;
+            int LegjobbFerfiMasodperc;
+            string LegjobbNoiVersenyzo;
+            int LegjobbNoiRajtszam;
+            int LegjobbNoiOra;
+            int LegjobbNoiPerc;
+            int LegjobbNoiMasodperc;
+            int min = 0;
+            int versenyido = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                versenyido = int.Parse(adatok[i].versenyido);
+                if (adatok[i].kategoria == "Ferfi")
+                {
+                    if (min < versenyido)
+                    {
+                        min = versenyido;
+                        LegjobbFerfiOra = adatok[i].ora;
+                        LegjobbFerfiPerc = adatok[i].perc;
+                        LegjobbFerfiMasodperc = adatok[i].masodperc;
+                        LegjobbFerfiVersenyzo = adatok[i].nev;
+                        LegjobbFerfiRajtszam = adatok[i].rajtszam;
+                    }
+                }
+                else if (adatok[i].kategoria == "Noi")
+                {
+                    if (min < versenyido)
+                    {
+                        min = versenyido;
+                        LegjobbNoiOra = adatok[i].ora;
+                        LegjobbNoiPerc = adatok[i].perc;
+                        LegjobbNoiMasodperc = adatok[i].masodperc;
+                        LegjobbNoiVersenyzo = adatok[i].nev;
+                        LegjobbNoiRajtszam = adatok[i].rajtszam;
+                    }
+                }
+            }
+            Console.WriteLine("8. Feladat: A verseny győztesei: ");
+            Console.WriteLine("Nők: {0} ({1}) - {2}:{3}:{5}", LegjobbNoiVersenyzo, LegjobbNoiRajtszam, LegjobbNoiOra, LegjobbNoiPerc, LegjobbNoiMasodperc);
+            Console.WriteLine("Férfiak: {0} ({1}) - {2}:{3}:{5}", LegjobbFerfiVersenyzo, LegjobbFerfiRajtszam, LegjobbFerfiOra, LegjobbFerfiPerc, LegjobbFerfiMasodperc);
+
+        }
+
+        private static void Feladat07()//1,1,1,1,1,0,1
+        {
+            double osszesen = 0;
+            double mennyi = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].kategoria == "Ferfi" && adatok[i].tavSzazalek == 100)
+                {
+                    osszesen = osszesen + adatok[i].ora + adatok[i].perc/60 + adatok[i].masodperc/3600;
+                    mennyi++;
+                }
+            }
+            double atlag = osszesen / mennyi;
+            Console.WriteLine("7. Feladat: Átlagos idő: {0} óra", atlag); 
+
+        }
+
+        private static void Feladat06()//1,2,1
+        {
+            int idoOraban = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                idoOraban= adatok[i].ora + adatok[i].perc/60 + adatok[i].masodperc/3600;
+            }
+        }
+
+        private static void Feladat05()
+        {
+            Console.Write("\n5. Feladat: Kérem a sportoló nevét: ");
+            string bekertVersenyzo = Console.ReadLine(); //1,1,0,1,1,1
+            int keresettTavszazalek = 0;
+            bool vane = false;
+            bool teljesitette = false;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].nev == bekertVersenyzo) //while
+                {
+                    vane = true;
+                    keresettTavszazalek = adatok[i].tavSzazalek;
+                    if (adatok[i].tavSzazalek == 100)
+                    {
+                        teljesitette = true;
+                    }
+                    break;
+                }
+            }
+            if (vane == true)
+            {
+                Console.WriteLine("Indult egyéniben a sportoló? Igen");
+                if (teljesitette == true)
+                {
+                    Console.WriteLine("Teljesítette a teljes távot? Igen");
+                }
+                else
+                {
+                    Console.WriteLine("Teljesítette a teljes távot? Nem");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Indult egyéniben a sportoló? Nem");
+            }
+        }
+
+        private static void Feladat04()//1,1,1,1,1
+        {
+            int mennyiCelbaertNoi = 0; 
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].kategoria == "Noi" && adatok[i].tavSzazalek == 100) 
+                {
+                    mennyiCelbaertNoi++;
+                }
+            }
+            Console.WriteLine("4. Feladat: Célba érkező női indulók: {0} fő", mennyiCelbaertNoi);
+        }
+
+        private static void Feladat03() //1,1
+        {
+            Console.WriteLine("3. Feladat: Egyéni indulók: {0} fő", adatok.Count);
+        }
+    }               //2,1,1,1,0,1,1,1,1
+}
